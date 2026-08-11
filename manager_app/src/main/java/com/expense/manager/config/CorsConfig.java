eae718e0660baf14a60c7261210368a5e1e39d97
Benson Zheng <bensonzheng2003@gmail.com>
2026-08-11 18:20:34 -0400

    .

--- manager_app/src/main/java/com/expense/manager/config/CorsConfig.java
+++ manager_app/src/main/java/com/expense/manager/config/CorsConfig.java
@@ -18,7 +18,9 @@ public class CorsConfig {
                 registry
                     .addMapping("/**")
                     .allowedOrigins(
-                        "http://localhost:5173"
+                        "http://localhost:5173",
+                        "http://frontend:5173",
+                        "http://manager-backend:9090"
                     )
                     .allowedMethods(
                         "GET",
