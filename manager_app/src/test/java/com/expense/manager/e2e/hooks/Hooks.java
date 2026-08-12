6f057b0edcdddfafa27ea73522367787e3e03958
Benson Zheng <bensonzheng2003@gmail.com>
2026-08-12 12:33:28 -0400

    .

--- manager_app/src/test/java/com/expense/manager/e2e/hooks/Hooks.java
+++ manager_app/src/test/java/com/expense/manager/e2e/hooks/Hooks.java
@@ -1,7 +1,12 @@
 package com.expense.manager.e2e.hooks;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -11,14 +16,27 @@ public class Hooks {
     public static WebDriver driver;
 
     @Before(order = 0)
-    public void setup() {
-        driver = new ChromeDriver();
+    public void setup() throws URISyntaxException, MalformedURLException {
+        String seleniumUrl = System.getenv("SELENIUM_URL");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        if (seleniumUrl != null && !seleniumUrl.isBlank()) {
+            driver = new RemoteWebDriver(new URI(seleniumUrl).toURL(), options);
+        } else {
+            // local fallback, e.g. running tests outside Docker
+            options.addArguments("--headless=new");
+            driver = new org.openqa.selenium.chrome.ChromeDriver(options);
+        }
+
         driver.manage().window().maximize();
     }
 
     @After(order = 0)
     public void teardown() {
-        if(driver != null) {
+        if (driver != null) {
             driver.quit();
             driver = null;
         }
